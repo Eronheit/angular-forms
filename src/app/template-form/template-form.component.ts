469b9623ccf50d934d7e1302939d37877315e53a
Eronheit <aquinoeronaldo@gmail.com>
2021-09-30 17:16:52 -0300

    feat: (template driven) search address in ViaCep API with CEP

--- src/app/template-form/template-form.component.ts
+++ src/app/template-form/template-form.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
 
 @Component({
   selector: 'app-template-form',
@@ -12,10 +13,28 @@ export class TemplateFormComponent implements OnInit {
     email: ''
   }
 
-  constructor() { }
+  constructor(private http: HttpClient) { }
 
   checkIsValidField(field: any) {
     return !field.valid && field.touched;
+  }
+
+  consultaCEP(cep: string) {
+    //Nova variável "cep" somente com dígitos.
+    cep = cep.replace(/\D/g, '');
+
+    //Verifica se campo cep possui valor informado.
+    if (cep != "") {
+      //Expressão regular para validar o CEP.
+      var validacep = /^[0-9]{8}$/;
+
+       //Valida o formato do CEP.
+       if(validacep.test(cep)) {
+        this.http.get(`https://viacep.com.br/ws/${cep}/json/`).subscribe(
+         (data:any) => console.log(data)
+        )
+       }
+    }
   }
 
   onSubmit(form: any){
